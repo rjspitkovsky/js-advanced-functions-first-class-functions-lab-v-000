e77983615b63939a4b3233d8f7df12191c967c24
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-03 23:21:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const returnLastTwoDrivers = function returnLastTwoDrivers(drivers) {
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
 function createFareMultiplier(integer) {
-  return function multiplier(fare) {
+  return function fareDoubler(fare) {
     return fare * integer
   }
 }
